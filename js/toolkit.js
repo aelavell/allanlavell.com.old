e6495e5c74060e7470e4027d0bfa0138a9dfb3e2
Allan Lavell <alavell@gmail.com>
2016-01-12 17:51:45 -0400

    Basic setup with codekit compilation of Bootstrap theme.

---
+++ js/toolkit.js
@@ -0,0 +1,16 @@
+// @codekit-append "bootstrap/affix.js";
+// @codekit-append "bootstrap/alert.js";
+// @codekit-append "bootstrap/button.js";
+// @codekit-append "bootstrap/carousel.js";
+// @codekit-append "bootstrap/collapse.js";
+// @codekit-append "bootstrap/dropdown.js";
+// @codekit-append "bootstrap/modal.js";
+// @codekit-append "bootstrap/tooltip.js";
+// @codekit-append "bootstrap/popover.js";
+// @codekit-append "bootstrap/scrollspy.js";
+// @codekit-append "bootstrap/tab.js";
+// @codekit-append "bootstrap/transition.js";
+// @codekit-append "custom/enter.js";
+// @codekit-append "custom/image-grid.js";
+// @codekit-append "custom/stage.js";
+// @codekit-append "custom/zoom.js";
